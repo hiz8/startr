149972d862cd36a6c3c68bd32e4b05893736b48a
hirofumi <detroit69city.nau@gmail.com>
2015-09-19 16:42:13 +0900

    add: editorconfig / clean: style change tab to space

--- provision.sh
+++ provision.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 if ! [ `which ansible` ]; then
-	wget http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6
-	sudo rpm --import RPM-GPG-KEY-EPEL-6
-	wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-	sudo rpm -ivh epel-release-6-8.noarch.rpm
-	sudo yum install -y ansible
+    wget http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6
+    sudo rpm --import RPM-GPG-KEY-EPEL-6
+    wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+    sudo rpm -ivh epel-release-6-8.noarch.rpm
+    sudo yum install -y ansible
 fi
 
 ssh-add
